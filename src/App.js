35630a91929450a69573b2d052262366f0944cda
Benarjishyam <benarji556612@gmail.com>
2023-12-10 16:57:36 +0530

    Add exisiting project files to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Class Component
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            message: "Hello World!"
+        };
+    }
+ 
+    componentDidMount() {
+        console.log("2.Mounting Stage method componentDidMount() called");
+    }
+ 
+    changeState() {
+        this.setState({ message: "Hey, The Message is changed to Hello Aliens..!" });
+        console.log("state changed")
+    }
+ 
+    render() {
+        return (
+            <div>
+                <h1>
+                    This is Demo for Component Lifecyle methods 
+                    Current Message:{this.state.message}
+                </h1>
+                <h2>
+                    <a
+                        onClick={this.changeState.bind(
+                            this
+                        )}
+                    >
+                        Press Here!
+                    </a>
+                </h2>
+            </div>
+        );
+    };
+ 
+    shouldComponentUpdate(nextProps, nextState) { //updated state and props
+        console.log("3.Update Phase shouldComponentUpdate()");
+        return true;
+    }
+ 
+    componentDidUpdate() {
+        console.log("3.Update Phase componentDidUpdate()");
+    }
+
+    componentWillUnmount(){
+        console.log("4. Unmounting Stage-> componentWillUnmount() called. ")
+    }
 }
 
 export default App;
